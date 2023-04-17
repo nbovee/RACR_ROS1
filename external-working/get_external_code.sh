1e33c9766cbe0fbd7a00cc4afd4312b732f4995a
nbovee <12849851+nbovee@users.noreply.github.com>
2023-04-17 14:30:56 -0400

    init file

---
+++ external-working/get_external_code.sh
@@ -0,0 +1,15 @@
+# git clone -q http://github.com/pal-robotics/ddynamic_reconfigure/ src/ddynamic_reconfigure/
+# git clone -q http://github.com/tu-darmstadt-ros-pkg/hector_slam src/hector_slam/
+# # git clone -q http://github.com/praveen-palanisamy/multiple-object-tracking-lidar src/multiple-object-tracking-lidar/
+# git clone -q http://github.com/ros-planning/navigation src/navigation/
+# # git clone -q -b 2.3.1 http://github.com/IntelRealSense/realsense-ros src/realsense-ros/
+# # git clone -q http://github.com/fazildgr8/ros_autonomous_slam src/ros_autonomous_slam/
+# # git clone -q http://github.com/dusty-nv/ros_deep_learning src/ros_deep_learning/
+# # git clone -q http://github.com/Slamtec/rplidar_ros src/rplidar_ros/
+# # git clone -q http://github.com/donrv/rrt-ros src/rrt-ros/
+# git clone -q http://github.com/wjwwood/serial src/serial/
+# git clone -q http://github.com/ros-teleop/teleop_twist_keyboard src/teleop_twist_keyboard/
+# git clone -q http://github.com/chiprobotics/chip_bldc_driver/ src/chip_bldc_driver/
+# git clone -q http://github.com/ros-drivers/usb_cam src/usb_cam/
+# # git clone -q http://github.com/dusty-nv/jetson-utils src/jetson-utils/
+# # git clone http://github.com/hasauino/rrt_exploration/ src/rrt_exploration/
