a4fc829bb675f244d2906dbeffd31276747d34a2
nbovee <12849851+nbovee@users.noreply.github.com>
2023-04-17 16:04:10 -0400

    move usb_cam to working

--- external-working/get_external_code.sh
+++ external-working/get_external_code.sh
@@ -3,3 +3,5 @@ git clone -q -b kinetic-devel http://github.com/pal-robotics/ddynamic_reconfigur
 git clone -q -b main http://github.com/wjwwood/serial src/serial/
 git clone -q -b master http://github.com/ros-teleop/teleop_twist_keyboard src/teleop_twist_keyboard/
 git clone -q -b melodic-devel http://github.com/tu-darmstadt-ros-pkg/hector_slam src/hector_slam/
+git clone -q -b melodic-devel http://github.com/ros-planning/navigation src/navigation/
+git clone -q -b develop http://github.com/ros-drivers/usb_cam src/usb_cam/
