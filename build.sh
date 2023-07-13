71e280c4447db929d9bc236f7aefa8b587c03f29
racr <racer@rowan.edu>
2023-07-13 17:49:42 -0400

    changes to get docker images building asap

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 if [ "$1" = prod ]; then
-    docker-compose -f docker-compose.yaml -f docker-compose.tx2.yaml build .
+    docker-compose -f docker-compose.yaml -f docker-compose.tx2.yaml build
 else
-    docker-compose build .
+    docker-compose build
 fi 
 
 if [ "$2" = run ] || [ "$1" = run ]; then
